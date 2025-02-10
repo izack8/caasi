fdc94c7d1284a46f9b8c243b0d3c00f7ece8c1d2
Izack Tay <isaactaypb@gmail.com>
2025-02-10 21:59:20 +0800

    configured FastAPI to be suitable for cloud deployment?

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="pages/static"), name="static")
+
+
+@app.get("/", response_class=FileResponse)
+async def read_root():
+    return FileResponse("pages/home.html")
+
+@app.get("/about", response_class=FileResponse)
+async def read_about():
+    return FileResponse("pages/about.html")
+
+@app.get("/about", response_class=FileResponse)
+async def read_contact():
+    return FileResponse("pages/contact.html")
