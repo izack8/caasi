770238a72cafc670c21e1f418eb57cc575e32cdc
Isaac Tay <isaactaypb@gmail.com>
2025-08-03 01:06:06 +0800

    fix: update footer credits with additional designers

--- frontend/src/app/components/ui/Footer.jsx
+++ frontend/src/app/components/ui/Footer.jsx
@@ -4,7 +4,10 @@ import SparkleText from '../animation/Sparkles';
 function Footer() {
   return (
     <footer className="lg:text-left text-slate-350 text-[10px] w-full mt-8">
-        <p>Coded by Isaac. Design inspired by <a href="https://alvinchang.dev">Alvin.</a></p>
+        <p>Coded by Isaac. Design inspired by
+        &nbsp;<a className="underline" href="https://alvinchang.dev">Alvin</a>,
+        &nbsp;<a className="underline" href="https://brittanychiang.com">Brittany</a>, and
+        &nbsp;<a className="underline" href="https://www.knlrvr.dev">Kane</a>.</p>
         &copy; 2025
         <div className='flex items-center gap-2'>
           <span>Click Me to Time Travel!</span>
