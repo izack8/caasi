9bced5e6a9a527e0348bb6d26c747d1b467ed624
izack8 <isaactaypb@gmail.com>
2026-02-04 17:29:43 +0800

    implemented backend for 'Project' document, since we have a whole page dedicated to fetch data from it

--- backend/src/controllers/project_controller.py
+++ backend/src/controllers/project_controller.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter
-from ..services.project_service import get_projects
+from ..services.project_service import get_project_by_id, get_projects
 
 router = APIRouter(prefix="/projects", tags=["Projects"])
 
 @router.get("")
 def fetch_projects():
     return get_projects()
+
+@router.get("/{project_id}")
+def fetch_project_by_id(project_id: str):
+    project = get_project_by_id(project_id)
+    if project:
+        return project
+    return {"error": "Project not found"}
--- backend/src/services/project_service.py
+++ backend/src/services/project_service.py
@@ -3,3 +3,7 @@ from ..mongo_db import db
 def get_projects():
     projects = list(db['projects'].find({}, {"_id": 0}))
     return projects
+
+def get_project_by_id(project_id):
+    project = db['projects'].find_one({"id": project_id}, {"_id": 0})
+    return project
