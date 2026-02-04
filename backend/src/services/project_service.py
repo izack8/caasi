86bc777785e678b04cdc21e47b0afdeca8214c2d
izack8 <isaactaypb@gmail.com>
2026-02-04 18:49:04 +0800

    fix: update project retrieval to use slug instead of id

--- backend/src/services/project_service.py
+++ backend/src/services/project_service.py
@@ -5,5 +5,7 @@ def get_projects():
     return projects
 
 def get_project_by_id(project_id):
-    project = db['projects'].find_one({"id": project_id}, {"_id": 0})
+    # we're using "slug", b/c i didnt define "id" / idw use ObjectId.
+    # maybe we should fix.
+    project = db['projects'].find_one({"slug": project_id}, {"_id": 0})
     return project
