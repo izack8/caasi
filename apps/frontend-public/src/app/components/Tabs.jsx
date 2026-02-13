333f3b7236a23252a2674922cf0ba5855628eee2
izack8 <isaactaypb@gmail.com>
2026-02-13 19:07:07 +0800

    safari issues

--- apps/frontend-public/src/app/components/Tabs.jsx
+++ apps/frontend-public/src/app/components/Tabs.jsx
@@ -20,6 +20,7 @@ function Tabs({ activeTab, onTabClick }) {
           key={tab.id}
           className={`tab bg-transparent border-none outline-none text-sm lg:text-base font-semibold relative pb-1 transition-colors duration-200 ${activeTab === tab.id ? 'text-black' : 'text-gray-500'} hover:text-black`}
           onClick={() => onTabClick(tab.id)}
+          style={{ WebkitTransform: 'translateZ(0)' }}
         >
           {tab.label}
           <span className={`absolute left-0 bottom-0 w-full h-[2px] transition-all duration-200 ${activeTab === tab.id ? 'bg-black' : 'bg-transparent'}`}></span>
