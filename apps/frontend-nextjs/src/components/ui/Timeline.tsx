8b79afdc3832a8cd467b797cd4dbc3d1b86a8636
izack8 <isaactaypb@gmail.com>
2026-02-19 06:50:03 +0800

    fixed ui bug

--- apps/frontend-nextjs/src/components/ui/Timeline.tsx
+++ apps/frontend-nextjs/src/components/ui/Timeline.tsx
@@ -1,4 +1,5 @@
 import type { Timeline as TimelineType } from '@/lib/types';
+import ReactMarkdown from 'react-markdown';
 
 interface TimelineProps {
   timelineData: TimelineType[];
@@ -38,7 +39,11 @@ function TimelineItem({ timelineData, isLastItem }: TimelineItemProps) {
 
       <div className="flex flex-col">
         <h1 className="text-lg font-semibold">{timelineData.timeline_date}</h1>
-        <p className="text-justify">{timelineData.timeline_description}</p>
+        <div className="text-justify">
+          <ReactMarkdown>
+            {timelineData.timeline_description}
+            </ReactMarkdown>
+        </div>
       </div>
 
         {/* if its the last item of array, dont render the line */}
