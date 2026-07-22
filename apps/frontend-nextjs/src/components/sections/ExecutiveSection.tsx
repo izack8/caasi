070a07ed846006e05b2f60e7e2c8bf25477a16ef
izack8 <isaactaypb@gmail.com>
2026-07-22 16:58:09 +0800

    omg so many changes lol

--- apps/frontend-nextjs/src/components/sections/ExecutiveSection.tsx
+++ apps/frontend-nextjs/src/components/sections/ExecutiveSection.tsx
@@ -15,11 +15,12 @@ function ExecutiveSection() {
                 <p>
                 Hello! My name is Isaac, or affectionately known as <b>izack/iz</b> by my friends. I'm a software engineer + ai/ml enthusiast, who loves exploring the intersection of technology and creativity, to build fun and creative projects.
                 </p>
+
+                <p>I am currently working as a software engineer at Singapore's national payment system, <span className="font-semibold"><a href="https://nets.com.sg" className="underline hover:text-rose-500 transition-colors duration-200" target="_blank" rel="noopener noreferrer">NETS</a></span>, building in-house workflow solutions for reporting and analytics, and further enhancing communication channels between <span className="font-semibold"><a href="https://en.wikipedia.org/wiki/List_of_banks_in_Singapore" className="underline hover:text-rose-500 transition-colors duration-200" target="_blank" rel="noopener noreferrer">local banks</a></span> :D.</p>
                 
                 <p>I recently built a <span className="font-semibold cursor-pointer hover:text-blue-500 transition-colors duration-200" onClick={() => router.push('/work/projects/sign-a-photo-jpg')}>web-app</span> that uses computer vision to detect the American Sign Language (ASL) alphabets, <a className="font-semibold cursor-pointer italic hover:text-rose-500 transition-colors duration-200" href="https://vt.tiktok.com/ZSmNxAXJR/" target="_blank" rel="noopener noreferrer">which garnered 710k+ views, 150k+ likes on TikTok</a>, and 500+ weekly page visits (which is the <b>craziest</b> thing ever).
                  </p>
 
-                 <p>I am currently working as a software engineer at Singapore's national payment system, <span className="font-semibold"><a href="https://nets.com.sg" className="underline hover:text-rose-500 transition-colors duration-200" target="_blank" rel="noopener noreferrer">NETS</a></span>, building in-house workflow solutions for reporting and analytics, and further enhancing communication channels between <span className="font-semibold"><a href="https://en.wikipedia.org/wiki/List_of_banks_in_Singapore" className="underline hover:text-rose-500 transition-colors duration-200" target="_blank" rel="noopener noreferrer">local banks</a></span> :D.</p>
 
                  <Image className="md:w-1/2 rounded-md" src="/images/intersection.png" alt="image of intersection of technology and creativity" width={500} height={500} />
             
